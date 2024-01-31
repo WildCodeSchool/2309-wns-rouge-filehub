2600f055ae125046fe71c1576599a7167df5d2de
FlavinouTheOne <flavien.dumas63@gmail.com>
2024-01-31 15:49:54 +0100

    full cleanup for clean start

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ start project :
 "docker compose up --build" to start server and DB,
 /frontent -> "npm run dev" to start frontend
 
-db --> ip adress: 0.0.0.0, port: 5433, db: thegoodcorner
+db --> ip adress: 0.0.0.0, port: 5433, db: filehub
--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,7 +1,4 @@
 import { DataSource } from "typeorm";
-import {Ad} from "./entities/Ad";
-import { Category } from "./entities/Category";
-import { Tag } from "./entities/Tag";
 import { User } from "./entities/User";
 
 export const dataSource = new DataSource({
@@ -11,7 +8,7 @@ export const dataSource = new DataSource({
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [Ad, Category, Tag, User],
+  entities: [User],
   synchronize: true,
   logging: false
 });
