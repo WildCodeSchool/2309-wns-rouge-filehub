7b1a5a611195f095a2d2b45d24639d8a7c7023ca
Daryl <chaigne.d01@gmail.com>
2024-02-01 14:09:00 +0100

    header casu good

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,15 @@
 import { useRouter } from "next/router";
+import Header from "@/layout/header";
 
 export default function Home(): React.ReactNode {
   const router = useRouter();
 
   return (
+      <div>
+      <Header />
     <>TEST</>
+      </div>
   );
 }
+
+
