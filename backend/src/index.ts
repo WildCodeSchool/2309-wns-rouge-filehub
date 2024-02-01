6a12c52323f0e75ab4e2b4be08be397c851881ae
Trung <trung.nguyenpro@hotmail.com>
2024-02-01 11:41:25 +0100

    style(Setup): 💄 Setup styledComponents with exemples

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,50 +1,52 @@
 import "reflect-metadata";
-import { dataSource } from "./datasource";
-import { buildSchema } from "type-graphql";
-import { ApolloServer } from "@apollo/server";
-import { UsersResolver } from "./resolvers/Users";
-import { ContextType, customAuthChecker } from "./auth";
-import { expressMiddleware } from '@apollo/server/express4';
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
-import express from 'express';
-import http from 'http';
-import cors from 'cors';
+import {dataSource} from "./datasource";
+import {buildSchema} from "type-graphql";
+import {ApolloServer} from "@apollo/server";
+import {UsersResolver} from "./resolvers/Users";
+import {ContextType, customAuthChecker} from "./auth";
+import {expressMiddleware} from "@apollo/server/express4";
+import {ApolloServerPluginDrainHttpServer} from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+import cors from "cors";
 
-async function start(){
+async function start() {
   await dataSource.initialize();
   const schema = await buildSchema({
     resolvers: [UsersResolver],
-    authChecker: customAuthChecker
-  })
+    authChecker: customAuthChecker,
+  });
 
   const app = express();
   const httpServer = http.createServer(app);
 
   const server = new ApolloServer<ContextType>({
     schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    plugins: [ApolloServerPluginDrainHttpServer({httpServer})],
   });
   await server.start();
 
   app.use(
-    '/',
+    "/",
     cors<cors.CorsRequest>({
       origin: "http://localhost:3000",
-      credentials: true
+      credentials: true,
     }),
-    express.json({ limit: '50mb' }),
+    express.json({limit: "50mb"}),
     expressMiddleware(server, {
-      context: async(args)=>{
+      context: async (args) => {
         return {
           req: args.req,
-          res: args.res
-        }
+          res: args.res,
+        };
       },
-    }),
+    })
   );
 
-  await new Promise<void>((resolve) => httpServer.listen({ port: 5000 }, resolve));
-  console.log(`🚀 Server ready at http://localhost:5000/`);
+  await new Promise<void>((resolve) =>
+    httpServer.listen({port: 5999}, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:5999/`);
 }
 
 start();
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,8 @@
-import { useRouter } from "next/router";
+import {TestStyled} from "@/components/testStyled";
+import {useRouter} from "next/router";
 
 export default function Home(): React.ReactNode {
   const router = useRouter();
 
-  return (
-    <>TEST</>
-  );
+  return <TestStyled />;
 }
