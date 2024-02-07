fb2256d3f81c2fe38cad60727e1ea4cdcbe0536e
remi59800 <remimenart@gmail.com>
2024-02-07 11:08:42 +0100

    added upload file on server and in bdd

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,19 +1,16 @@
-import {useRouter} from "next/router";
-import Header from "@/layout/header";
-import {TestStyled} from '@/components/TestStyled';
-
+import { useRouter } from 'next/router';
+import Header from '@/layout/header';
+import { TestStyled } from '@/components/TestStyled';
+import { UploadFile } from '@/components/UploadFile';
 
 export default function Home(): React.ReactNode {
-    const router = useRouter();
+  const router = useRouter();
 
-
-    return (
-        <>
-            <Header/>
-            <TestStyled/>
-        </>
-
-    )
+  return (
+    <>
+      <Header />
+      <TestStyled />
+      <UploadFile />
+    </>
+  );
 }
-
-
