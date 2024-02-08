dccbc6484fa669b2d1b8e0b538e0a75bca6df7f0
remi59800 <remimenart@gmail.com>
2024-02-08 17:27:07 +0100

    WIP test jest backend

--- backend/src/datasource.ts
+++ backend/src/datasource.ts
@@ -1,15 +1,20 @@
-import { DataSource } from "typeorm";
-import { User } from "./entities/User";
-import { File } from "./entities/File";
+import { DataSource } from 'typeorm';
+import { User } from './entities/User';
+import { File } from './entities/File';
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+export const dataSourceOptions: PostgresConnectionOptions = {
+  type: 'postgres',
+  entities: [File, User],
+  synchronize: true,
+  logging: true,
+};
 
 export const dataSource = new DataSource({
-  type: "postgres",
-  host: "db",
+  ...dataSourceOptions,
+  host: 'db',
   port: 5432,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: [User, File],
-  synchronize: true,
-  logging: false,
 });
