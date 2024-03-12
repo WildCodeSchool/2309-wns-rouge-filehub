69dc2cdca242f2041e4ef300f3df11f5919c8565
Trung <trung.nguyenpro@hotmail.com>
2024-03-12 17:10:00 +0100

    fix test

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -9,9 +9,6 @@ const createJestConfig = nextJest({
 const customJestConfig = {
     setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
     testEnvironment: "jsdom",
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/$1',
-      },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
--- frontend/src/__test__/login.test.tsx
+++ frontend/src/__test__/login.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
-import LoginPage from "@/pages/login";
+import LoginPage from "../pages/login";
 import Login from "@/components/Login";
 import { MockedProvider } from "@apollo/client/testing";
 
