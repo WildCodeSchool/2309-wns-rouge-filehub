1efcbe99a819a0037a8386e4b2f48cba8fa17604
Trung <trung.nguyenpro@hotmail.com>
2024-03-13 15:59:52 +0100

    test ci lint

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ import { UploadFileController } from './controllers/UploadFile';
 import { DownloadFileController } from './controllers/DownloadFile';
 import { getSchema } from './schema';
 
-async function start() {
+asynca function start() {
   await dataSource.initialize();
 
   const schema = await getSchema();
--- frontend/src/components/Login.tsx
+++ frontend/src/components/Login.tsx
@@ -81,7 +81,7 @@ export default function Login(): React.ReactNode {
   const [signin] = useMutation(mutationSignin);
   const [signup] = useMutation(mutationSignup);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  consta handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     if (activeTab === 0) {
       if (signinEmail && signinPassword) {
