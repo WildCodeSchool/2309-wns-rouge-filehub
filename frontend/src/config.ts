0bf9fc7fa90260b3fbee7e8104b004286441d6b4
FlavinouTheOne <flavien.dumas63@gmail.com>
2024-04-18 14:48:23 +0200

    fixed API_URL for local dev AND server prod

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,1 +1,4 @@
-export const API_URL = process.env.API_URL || "http://localhost:5001";
+export const API_URL = (typeof window !== 'undefined' && window.location.origin.includes("localhost") === false) ?
+  `${window.location.origin}/graphql`
+  :
+  "http://localhost:5001";
