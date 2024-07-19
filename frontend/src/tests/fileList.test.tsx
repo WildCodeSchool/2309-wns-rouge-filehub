36ea425d408167d7e6915c6b568fc76b5d27912c
Pierre Quignon <p.quignon.pro@gmail.com>
2024-07-19 12:13:25 +0200

    edit and implement FileList test

--- frontend/src/tests/fileList.test.tsx
+++ frontend/src/tests/fileList.test.tsx
@@ -1,101 +1,189 @@
-import { MockedProvider } from "@apollo/client/testing";
-import { queryMe } from "../graphql/queryMe";
-import { getUserFiles } from "../graphql/getUserFiles";
-import FileList, { fixedHeaderContent } from "@/components/FileList";
-import { render, waitFor } from "@testing-library/react";
-import { VirtuosoProps } from "react-virtuoso";
 import React from "react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import { toast } from "react-toastify";
+import axios from "axios"; // Ajout de l'importation d'axios
+import FileList from "@/components/FileList";
 
-jest.mock("next/router", () => require("next-router-mock"));
+import { mocks } from "./mockFileList";
 
-jest.mock("react-virtuoso", () => {
-  function TableVirtuoso(props: VirtuosoProps<unknown, unknown>) {
-    return (
-      <table>
-        <thead>{fixedHeaderContent()}</thead>
-        <tbody>
-          {props.data?.map((value, index) => (
-            <tr key={index}>{props.itemContent?.(index, value, undefined)}</tr>
-          ))}
-        </tbody>
-      </table>
-    );
-  }
+// Mock pour toastify
+jest.mock("react-toastify", () => ({
+  toast: {
+    success: jest.fn(),
+    error: jest.fn(),
+  },
+}));
 
-  return { ...jest.requireActual("react-virtuoso"), TableVirtuoso };
+// Mock de l'API Clipboard
+Object.assign(navigator, {
+  clipboard: {
+    writeText: jest.fn().mockImplementation(() => Promise.resolve()),
+  },
 });
 
-const mocks = [
-  {
-    request: {
-      query: queryMe,
-    },
-    result: {
-      data: {
-        me: {
-          id: "123",
-          email: "test@test.test",
-        },
-      },
-    },
-  },
-  {
-    request: {
-      query: getUserFiles,
-      variables: {
-        userId: "123",
-      },
-    },
-    result: {
-      data: {
-        filesCurrentUser: [
-          {
-            id: "58",
-            mimeType: "image/png",
-            originalName: "fichier1.png",
-            uniqueName: "1713515641747fichier1.png",
-            path: "/app/src/Files/1713515641747fichier1.png",
-            size: 128954,
-            uploadAt: "2024-04-19T08:34:01.761Z",
-            url: "http://localhost:3000/downloads/1713515641747fichier1.png",
-            createdBy: {
-              id: "123",
-              email: "test@test.test",
-              __typename: "User",
-            },
-            __typename: "File",
-          },
-          {
-            id: "59",
-            mimeType: "image/png",
-            originalName: "fichier2.png",
-            uniqueName: "1713515641747fichier2.png",
-            path: "/app/src/Files/1713515641747fichier2.png",
-            size: 128954,
-            uploadAt: "2024-04-19T08:34:01.761Z",
-            url: "http://localhost:3000/downloads/1713515641747fichier2.png",
-            createdBy: {
-              id: "123",
-              email: "test@test.test",
-              __typename: "User",
-            },
-            __typename: "File",
-          },
-        ],
-      },
-    },
-  },
-];
+// Mock d'axios
+jest.mock("axios");
 
-describe("rendering FileList component with mock data", () => {
-  it("should render TableVirtuoso with user files from mock data", async () => {
-    const result = render(
-      <MockedProvider mocks={mocks} addTypename={false}>
+describe("FileList", () => {
+  // Nettoyer les mocks avant chaque test
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("Display files list", async () => {
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
         <FileList />
       </MockedProvider>,
     );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file5.pdf")).toBeInTheDocument(),
+    );
+  });
+
+  it("handles file deletion", async () => {
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    const deleteButtons = screen.queryAllByTestId("delete");
+    fireEvent.click(deleteButtons[0]);
+
+    await waitFor(() =>
+      expect(toast.success).toHaveBeenCalledWith(
+        "Fichier supprimé avec succès!",
+      ),
+    );
+
     await waitFor(() => {
-      expect(result.getByText("fichier2.png")).toBeInTheDocument();
+      const fileName = screen.queryByText("file1.pdf");
+      expect(fileName).not.toBeInTheDocument();
     });
+  });
+
+  it("handles file download", async () => {
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
+      data: new Blob(["dummy content"], { type: "application/pdf" }),
+      headers: {
+        "content-type": "application/pdf",
+      },
+    });
+
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    global.URL.createObjectURL = jest.fn(() => "blob:dummy-url");
+    const downloadButton = screen.queryAllByTestId("download");
+    fireEvent.click(downloadButton[0]);
+
+    await waitFor(() =>
+      expect(toast.success).toHaveBeenCalledWith(
+        "Fichier téléchargé avec succès!",
+      ),
+    );
+  });
+
+  it("handles copying link to clipboard", async () => {
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    const copyButton = screen.queryAllByTestId("copy");
+    fireEvent.click(copyButton[0]);
+
+    await waitFor(() =>
+      expect(toast.success).toHaveBeenCalledWith("Lien copié avec succès!"),
+    );
+
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith(
+      "http://example.com/files/unique-file1.pdf",
+    );
+  });
+  it("opens file when it's an image or a pdf file", async () => {
+    // Mock la réponse d'axios pour le téléchargement du fichier
+    (axios.get as jest.MockedFunction<typeof axios.get>).mockResolvedValue({
+      data: new Blob(["dummy content"], { type: "application/pdf" }),
+      headers: {
+        "content-type": "application/pdf",
+      },
+    });
+
+    // Mock URL.createObjectURL
+    global.URL.createObjectURL = jest.fn(() => "blob:dummy-url");
+
+    // Mock window.open
+    const openSpy = jest.spyOn(window, "open").mockImplementation(() => null);
+
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    const openButtons = screen.queryAllByTestId("open");
+    fireEvent.click(openButtons[0]);
+
+    await waitFor(() => {
+      expect(global.URL.createObjectURL).toHaveBeenCalled();
+      expect(openSpy).toHaveBeenCalledWith("blob:dummy-url", "_blank");
+    });
+
+    // Nettoyage
+    openSpy.mockRestore();
+  });
+
+  it("navigate to the second page", async () => {
+    render(
+      <MockedProvider mocks={mocks(false)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file1.pdf")).toBeInTheDocument(),
+    );
+
+    const nextPageButton = screen.getByRole("button", { name: /next page/i });
+
+    fireEvent.click(nextPageButton);
+
+    render(
+      <MockedProvider mocks={mocks(true)} addTypename={false}>
+        <FileList />
+      </MockedProvider>,
+    );
+
+    await waitFor(() =>
+      expect(screen.getByText("file6.pdf")).toBeInTheDocument(),
+    );
   });
 });
